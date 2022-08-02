c196ab44abcaa6cbab28b065f3681614f738dbba
Recondito <santanallanes@hotmail.com>
2022-08-02 17:45:17 -0400

    Implemented test real time events.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,28 @@
 const path = require('path')
+const http = require('http')
 const express = require('express')
+const socketio = require('socket.io')
 
 const app = express()
+const server = http.createServer(app)
+const io = socketio(server)
 
 const port = process.env.PORT || 3000
 const publicDirectoryPath = path.join(__dirname, '../public')
 
 app.use(express.static(publicDirectoryPath))
 
-app.listen(port, () => {
+let count = 0
+
+io.on('connection', (socket) => {
+    console.log('New WebSocket Connection.')
+    socket.emit('countUpdated', count)
+    socket.on('increment', () => {
+        count++
+        io.emit('countUpdated', count)
+    })
+})
+
+server.listen(port, () => {
     console.log(`Server is up on ${port}.`)
 })
