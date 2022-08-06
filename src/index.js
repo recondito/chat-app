a57c73f046f28e14c0cb22461875f3db12c38a2f
Recondito <santanallanes@hotmail.com>
2022-08-06 11:38:53 -0400

    Removed Test Code. Added EventListener for msgs.

--- src/index.js
+++ src/index.js
@@ -12,14 +12,13 @@ const publicDirectoryPath = path.join(__dirname, '../public')
 
 app.use(express.static(publicDirectoryPath))
 
-let count = 0
-
 io.on('connection', (socket) => {
     console.log('New WebSocket Connection.')
-    socket.emit('countUpdated', count)
-    socket.on('increment', () => {
-        count++
-        io.emit('countUpdated', count)
+
+    socket.emit('message', 'Welcome!')
+
+    socket.on('sendMessage', (message) => {
+        io.emit('message', message)
     })
 })
 
